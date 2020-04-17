945de276f02275ea05bafcb55e369457c26f45e8
selimssevgi <selimssevgi@gmail.com>
2020-04-17 19:23:33 +0200

    feat: abstract over snackbar service for simplicity

---
+++ src/app/shared/services/notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  onLeftBottomOk(message: string) {
+    this.snackBar.open(
+      message,
+      'OK',
+      { horizontalPosition: 'left', duration: 5000 });
+  }
+
+  somethingUnexpectedHappen() {
+    this.onLeftBottomOk('Something unexpected happened. Try again later.');
+  }
+}
