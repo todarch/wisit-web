f16ad45cb0b144eb6d08aa5a7be91486435be939
selimssevgi <selimssevgi@gmail.com>
2020-04-28 23:27:43 +0200

    fix: use just a button for sign in/sign up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,6 @@ const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
   { path: 'login', component: DummyComponent, canActivate: [AppAuthGuard] },
-  { path: 'sign-up', component: DummyComponent, canActivate: [AppAuthGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
