33afa98aaaaed8ee2b9f36a0902d025cf7138d13
selimssevgi <selimssevgi@gmail.com>
2020-05-02 17:25:44 +0200

    feat: challenge user on welcome page

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
+export class WelcomeComponent implements OnInit, OnDestroy {
+  picUrl = 'https://img.delicious.com.au/IfD4cIOg/del/2018/12/copenhagen-denmark-97411-2.jpg';
+  choices = ['riga', 'prague', 'kyiv', 'copenhagen'];
+  private colors = ['antiquewhite', 'azure', 'beige'];
+  private index = 0;
+  color = this.colors[this.index];
+
+  private colorChanger;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.colorChanger = setInterval(() => {
+      if ((this.index + 1) === this.colors.length) {
+        this.index = 0;
+      } else {
+        this.index++;
+      }
+      this.color = this.colors[this.index];
+    }, 1000);
   }
+
+  ngOnDestroy() {
+    if (this.colorChanger) {
+      clearInterval(this.colorChanger);
+    }
+  }
+
 
 }
